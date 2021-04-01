db3313a753e4caa9fa718d99ffecdcb5f8a23c3d
AlexDjangoX <alexmonk17@icloud.com>
2021-04-01 20:46:40 +0200

    Added Procfile

--- dentist/settings.py
+++ dentist/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['alexdjangoblogapp.herokuapp.com']
+ALLOWED_HOSTS = ['https://alexdjangoxdentist.herokuapp.com']
 
 
 # Application definition
